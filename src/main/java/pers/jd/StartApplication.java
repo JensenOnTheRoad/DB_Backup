46e793d1e2b2d146badfa2cfaeade92742088ed8
jensen_deng <jensendengwork@163.com>
2022-06-07 09:40:15 +0800

    :tada: project initialization

---
+++ src/main/java/pers/jd/StartApplication.java
@@ -0,0 +1,14 @@
+package pers.jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jensen
+ */
+@SpringBootApplication
+public class StartApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(StartApplication.class, args);
+  }
+}
